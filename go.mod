faf87ac3a50910e7767bf97889121211200cce0a
Slathian <joshua@angelo.net>
2024-05-13 11:26:26 -0700

    added dog dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Slathian/puppy
 
 go 1.22.1
+
+require github.com/Slathian/Dog v0.0.0-20240513182329-356a965a002a // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	dog "github.com/Slathian/Dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof Woof! Woof Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
